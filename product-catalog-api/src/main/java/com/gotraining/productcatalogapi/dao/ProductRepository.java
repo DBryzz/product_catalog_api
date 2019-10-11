339c701d948cd88b409ada5fe16d86c593a7f08d
= <=>
2019-10-11 14:25:31 +0100

    massive delete

--- product-catalog-api/src/main/java/com/gotraining/productcatalogapi/dao/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gotraining.productcatalogapi.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gotraining.productcatalogapi.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
